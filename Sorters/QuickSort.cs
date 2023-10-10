b5d14f013933f16eedce89e90e1abeb13b25298b
6PaTeL <jkdjkd789@gmail.com>
2023-10-10 17:15:08 +0300

    Added QuickSort

---
+++ Sorters/QuickSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorters
+{
+    public class QuickSort<T> : AlgortihtmBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            Qsort(0, Items.Count - 1);    
+        }
+
+        private void Qsort(int left, int right) 
+        {
+            if(left >= right) 
+            {
+                return; 
+            }
+
+            var pivot = Sorting(left, right);
+
+            Qsort(left, pivot - 1);
+            Qsort(pivot + 1, right); 
+        }
+
+        private int Sorting(int left, int right) 
+        {
+            var pointer = left;
+
+            //Опорный емелент находится под Right - адресом
+            for(int i = left; i <= right; i++) 
+            {
+                if (Items[i].CompareTo(Items[right]) == -1) 
+                {
+                    Swap(pointer, i);
+                    pointer++; 
+                }
+            }
+
+            Swap(pointer, right);
+            return pointer; 
+        }
+    }
+}
---
+++ SortersTests/QuickSortTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorters.Tests
+{
+    [TestClass()]
+    public class QuickSortTests
+    {
+        [TestMethod()]
+        public void MakeSortTest()
+        {
+            var quick = new QuickSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (int i = 0; i < 800; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+
+            quick.Items.AddRange(items);
+            var sorted = items.OrderBy(x => x).ToArray();
+
+            //act
+            quick.Sort();
+
+            //assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(sorted[i], quick.Items[i]);
+            }
+        }
+    }
+}
